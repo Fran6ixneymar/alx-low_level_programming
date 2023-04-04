38be4989a48d695cd2cac4ab673dad678d8ec538
fran6ixneymar <ifebukofrancis@gmail.com>
2023-04-04 16:43:48 -0700

    SINGLY

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,15 +9,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
- unsigned int fig = 0;
- listint_t *start = head;
+unsigned int fig = 0;
+listint_t *start = head;
 
- while (start && fig < index)
- {
- start = start->next;
- fig++;
- }
-
- return (start ? start : NULL);
+while (start && fig < index)
+{
+start = start->next;
+fig++;
 }
 
+return (start ? start : NULL);
+}
